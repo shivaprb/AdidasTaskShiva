0080f7522d8d3acaa38d102a6c70cbe0cecb29ed
shiva <shivapourbehzad@gmail.com>
2021-09-24 00:37:05 +0200

    Paralel Test, Failed Test, Reporting in this Branch

--- src/test/java/com/adidas/runners/CukesRunner.java
+++ src/test/java/com/adidas/runners/CukesRunner.java
@@ -7,6 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
+        plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/Features",
         glue = "com/adidas/stepdefinitions",
         dryRun = false,
