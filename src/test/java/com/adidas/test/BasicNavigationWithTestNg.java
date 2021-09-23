0fb2effa6691bb5688a357f44bf1bc8576d2d0e9
shiva <shivapourbehzad@gmail.com>
2021-09-24 00:42:17 +0200

    TestNG Branch updated02

--- src/test/java/com/adidas/test/BasicNavigationWithTestNg.java
+++ src/test/java/com/adidas/test/BasicNavigationWithTestNg.java
@@ -1,2 +1,31 @@
-package com.adidas.test;public class BasicNavigationWithTestNg {
+package com.adidas.test;
+
+
+import com.adidas.TestBaseAdidas;
+import com.adidas.pages.AdidasHomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class BasicNavigationWithTestNg extends TestBaseAdidas {
+    @Test
+
+    public void testNavigation() {
+
+
+        AdidasHomePage homePage = new AdidasHomePage();
+
+
+        homePage.Laptops.click();
+
+        wait.until(ExpectedConditions.visibilityOf(homePage.Sony));
+
+        Assert.assertTrue(homePage.Sony.isDisplayed());
+
+
+    }
 }
